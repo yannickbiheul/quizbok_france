68c5a74a7415b6a25109681b2352ab43c387b09d
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-28 07:49:29 +0200

    Ajout du bouton et menu Burger

---
+++ js/quizBok_France.js
@@ -0,0 +1,20 @@
+/* MENU BURGER */
+let boutonBurger = document.querySelector(".boutonBurger");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
+let menuBurger = document.querySelector(".menuBurger");
+let elts = document.querySelectorAll(".elt");
+
+function toggleMenu() {
+    menuBurger.classList.toggle("menuBurgerOpen");
+    span1.classList.toggle("span1Open");
+    span2.classList.toggle("span2Open");
+    span3.classList.toggle("span3Open");
+}
+
+boutonBurger.addEventListener("click", toggleMenu);
+
+for (let i = 0; i < elts.length; i++) {
+    elts[i].addEventListener("click", toggleMenu);
+}
