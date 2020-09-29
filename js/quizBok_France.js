cf49d34fa1fe514854a8f969ccae785fd414b5e6
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-29 08:04:16 +0200

    Changement nav, ajouts objets questions en javascript

--- js/quizBok_France.js
+++ js/quizBok_France.js
@@ -18,3 +18,49 @@ boutonBurger.addEventListener("click", toggleMenu);
 for (let i = 0; i < elts.length; i++) {
     elts[i].addEventListener("click", toggleMenu);
 }
+
+/* QUESTIONS */
+let question1 = {
+    question: "Combien y'a t-il d'habitants en France (environ) ?",
+    reponse1: "57 Millions",
+    reponse2: "67 Millions",
+    reponse3: "77 Millions",
+    reponse4: "87 Millions",
+    bonneReponse: "67 Millions"
+};
+
+let question2 = {
+    question: "Quel président ne fait pas partie de la cinquième république ?",
+    reponse1: "Alain Poher",
+    reponse2: "François Mitterand",
+    reponse3: "François Hollande",
+    reponse4: "René Coty",
+    bonneReponse: "René Coty"
+};
+
+let question3 = {
+    question: "À quel département correspond le code 41 ?",
+    reponse1: "Jura",
+    reponse2: "Landes",
+    reponse3: "Loire-et-Cher",
+    reponse4: "Loire",
+    bonneReponse: "Loire-et-Cher"
+};
+
+let question4 = {
+    question: "À quel peintre français doit-on le tableau \"Le radeau de la Méduse\" ?",
+    reponse1: "Claude Monet",
+    reponse2: "Paul Cezanne",
+    reponse3: "Jean Louis Théodore Géricault",
+    reponse4: "Jean Baptiste Corot",
+    bonneReponse: "Jean Louis Théodore Géricault"
+};
+
+let question5 = {
+    question: "Quel est le plus long fleuve français ?",
+    reponse1: "Le Rhône",
+    reponse2: "La Loire",
+    reponse3: "La Seine",
+    reponse4: "L' Odet",
+    bonneReponse: "La Loire"
+};
